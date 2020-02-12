af985160cf310d9ae4153bc928e961a81c08eabe
Королев Сергей <serg_2004@inbox.ru>
2020-02-12 17:18:08 +0700

    небольшие правки

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=en,
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language: es or fr")
 
 
@@ -13,6 +13,6 @@ def browser(request):
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome()
     yield browser
     browser.quit()
